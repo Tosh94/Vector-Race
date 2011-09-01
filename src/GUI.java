48d39a929e65c8b3f8b4058b6312ad0ca174f21e
Bastian Jonas <bastian.j90@gmx.de>
2011-09-01 16:20:37 +0200

    Map-readin implemented

--- src/GUI.java
+++ src/GUI.java
@@ -7,12 +7,11 @@ public class GUI extends JFrame implements KeyListener{
 	Can can;
 	JMenuBar menu;
 	JMenu file, options, help;
-	JLabel turn, speedLbl, actualVectorLbl, roadVectorLbl, speed, actualVector, roadVector;
-	Player[][] players;
-	int actualTurn, status, cursorX, cursorY;
+	JLabel turn, speedLbl, actualVectorLbl, roadVectorLbl, speed, actualVector, roadVector, mapName;
+	Player[] players;
+	int status, cursorX, cursorY;
 	JFileChooser loadMap, loadSave;
 	RandomAccessFile reader;
-	String mapname;
 	
 	public GUI()
 	{
@@ -44,7 +43,7 @@ public class GUI extends JFrame implements KeyListener{
 		menu.add(help);
 		setJMenuBar(menu);
 		
-		turn=new JLabel("X´s Turn");
+		turn=new JLabel("");
 		turn.setBounds(0,30,265,20);
 		add(turn);
 		turn.setHorizontalAlignment(SwingConstants.CENTER);
@@ -54,7 +53,7 @@ public class GUI extends JFrame implements KeyListener{
 		add(actualVectorLbl);
 		actualVectorLbl.setHorizontalAlignment(SwingConstants.CENTER);
 		
-		actualVector=new JLabel("(  0    0  )");
+		actualVector=new JLabel("");
 		actualVector.setBounds(0,130,265,20);
 		add(actualVector);
 		actualVector.setHorizontalAlignment(SwingConstants.CENTER);
@@ -64,7 +63,7 @@ public class GUI extends JFrame implements KeyListener{
 		add(speedLbl);
 		speedLbl.setHorizontalAlignment(SwingConstants.CENTER);
 		
-		speed=new JLabel(calcSpeed(0,0));
+		speed=new JLabel("");
 		speed.setBounds(0,230,265,20);
 		add(speed);
 		speed.setHorizontalAlignment(SwingConstants.CENTER);
@@ -74,10 +73,16 @@ public class GUI extends JFrame implements KeyListener{
 		add(roadVectorLbl);
 		roadVectorLbl.setHorizontalAlignment(SwingConstants.CENTER);
 		
-		roadVector=new JLabel("(  0    0  )");
+		roadVector=new JLabel("");
 		roadVector.setBounds(0,330,265,20);
 		add(roadVector);
 		roadVector.setHorizontalAlignment(SwingConstants.CENTER);
+		
+		mapName=new JLabel("");
+		mapName.setBounds(265,515,500,20);
+		add(mapName);
+		
+		setLabels();
 		
 		Action newGame = new AbstractAction( "New..." ) {
 			  public void actionPerformed( ActionEvent e ) {
@@ -88,16 +93,15 @@ public class GUI extends JFrame implements KeyListener{
 						  String answer=JOptionPane.showInputDialog("Spielerzahl angeben:");
 						  if(answer==null) break;
 						  int playerNumber=Integer.parseInt(answer);
-						  players=new Player[playerNumber][1];
+						  players=new Player[playerNumber];
 						  for(int i=0;i< playerNumber;i++)
 						  {
-							  players[i][0]=new Player(i+1,10,10,0,1,0);
+							  players[i]=new Player(i+1,10,10,0,1,0);
 						  }
 						  status=1;
-						  actualTurn=1;
 						  setLabels();
-						  cursorX=players[actualTurn][0].getX();
-						  cursorY=players[actualTurn][0].getY();
+						  cursorX=players[status-1].getX();
+						  cursorY=players[status-1].getY();
 						  int returnVal=loadMap.showDialog(null, "Load...");
 						  if(returnVal==JFileChooser.APPROVE_OPTION)
 						  {
@@ -204,7 +208,12 @@ public class GUI extends JFrame implements KeyListener{
 	@Override
 	public void keyPressed(KeyEvent e) 
 	{
-		System.out.println(e.getKeyText(e.getKeyCode()));
+		System.out.println(e.getKeyCode());
+		if(status!=0)
+		{
+			int x=players[status-1].getX();
+			System.out.println(x);
+		}
 	}
 
 	@Override
@@ -221,10 +230,9 @@ public class GUI extends JFrame implements KeyListener{
 	
 	public void useMap(String mapPath) throws Exception
 	{
-		System.out.println(mapPath);
-		reader=new RandomAccessFile("mapPath","r");
+		reader=new RandomAccessFile(mapPath,"r");
 		String readin=reader.readLine();
-		mapname=readin;
+		mapName.setText(readin);
 		String map="";
 		while(readin!=null)
 		{
@@ -241,6 +249,7 @@ public class GUI extends JFrame implements KeyListener{
 		System.out.println(loadedGame);
 		reader=new RandomAccessFile("loadedGame","r");
 		String readin=reader.readLine();
+		@SuppressWarnings("unused")
 		String save=readin;
 		while(readin!=null)
 		{
@@ -260,8 +269,20 @@ public class GUI extends JFrame implements KeyListener{
 	
 	public void setLabels()
 	{
-		turn.setText("Player " + actualTurn + "´s turn");
-		actualVector.setText("(  " + players[actualTurn-1][players[actualTurn-1].length-1].getVx() + "    " + players[actualTurn-1][players[actualTurn-1].length-1].getVy() + "  )");
-		speed.setText(calcSpeed(players[actualTurn-1][players[actualTurn-1].length-1].getVx(),players[actualTurn-1][players[actualTurn-1].length-1].getVy()));
+		if(status!=0)
+		{
+			turn.setText("Player " + status + "´s turn");
+			actualVector.setText("(  " + players[status-1].getVx() + "    " + players[status-1].getVy() + "  )");
+			speed.setText(calcSpeed(players[status-1].getVx(),players[status-1].getVy()));
+			roadVector.setText("(  0    0  )");
+		}
+		else
+		{
+			turn.setText("Spiel nicht gestartet");
+			actualVector.setText("");
+			speed.setText("");
+			roadVector.setText("");
+			
+		}
 	}
 }
